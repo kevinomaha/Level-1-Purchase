9beef1c31e2aa9ca51887954c96cb16693d18b15
lsamban <lsamban@giftcertificates.com>
2014-11-06 13:44:39 -0600

    Test commit

--- app/js/controllers/GC/customizationCtrl.js
+++ app/js/controllers/GC/customizationCtrl.js
@@ -1,4 +1,4 @@
-four51.app.controller('CustomizationCtrl', ['$routeParams', '$sce', '$scope', '$451', '$rootScope', '$location',
-function ($routeParams, $sce, $scope, $451, $rootScope, $location) {
+four51.app.controller('CustomizationCtrl', ['$routeParams', '$sce', '$scope', '$451', '$rootScope', '$location','$ngResource',
+function ($routeParams, $sce, $scope, $451, $rootScope, $location, $ngResource) {
 
 }]);
