9fce545b7460d0f212fcaaac99d7c759b16f032a
lsamban <lsamban@giftcertificates.com>
2014-11-10 11:46:18 -0600

    check1

--- app/js/controllers/GC/customizationCtrl.js
+++ app/js/controllers/GC/customizationCtrl.js
@@ -1,6 +1,11 @@
-four51.app.controller('CustomizationCtrl', ['$routeParams', '$sce', '$scope', '$451', '$rootScope', '$location', 'Customization',
-function ($routeParams, $sce, $scope, $451, $rootScope, $location, Customization) {
+four51.app.controller('CustomizationCtrl', ['$routeParams', '$sce', '$scope', '$451', '$rootScope', '$location', 'Customization', '$http',
+function ($routeParams, $sce, $scope, $451, $rootScope, $location, Customization, $http) {
 
     $scope.selectedEmployee = Customization.getEmployee();
     console.log($scope.selectedEmployee); //fill in inputs with employee details
+
+    $http.get('http://GCA-SVCS01-DEV.cloudapp.net/DigitalTemplate/GetTemplateThumbnails/SCD002-GC1-02/1').
+        success(function(data){
+            $scope.getTemplate = data;
+        });
 }]);
