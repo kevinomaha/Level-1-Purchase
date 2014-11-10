d7d0bf4a7a5b070542f183b67508b6a27069767c
lsamban <lsamban@giftcertificates.com>
2014-11-10 16:20:15 -0600

    UI

--- app/js/controllers/GC/customizationCtrl.js
+++ app/js/controllers/GC/customizationCtrl.js
@@ -6,7 +6,11 @@ function ($routeParams, $sce, $scope, $451, $rootScope, $location, Customization
 
     $http.get('https://gca-svcs01-dev.cloudapp.net/ClientService/GetTemplateThumbnails?s=SCD002-GC1-02&o=1').
         success(function(data){
-            console.log(data);
+
             $scope.getTemplate = data;
+            console.log($scope.getTemplate.DesignId);
+            console.log($scope.getTemplate.Name);
+            console.log($scope.getTemplate.ThumbNailUrl);
+
         });
 }]);
