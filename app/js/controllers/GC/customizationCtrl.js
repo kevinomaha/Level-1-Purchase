fc1ee8b6b2305d34b20756454fb22e366c86d575
lsamban <lsamban@giftcertificates.com>
2014-11-10 15:15:57 -0600

    template service

--- app/js/controllers/GC/customizationCtrl.js
+++ app/js/controllers/GC/customizationCtrl.js
@@ -4,7 +4,7 @@ function ($routeParams, $sce, $scope, $451, $rootScope, $location, Customization
     $scope.selectedEmployee = Customization.getEmployee();
     console.log($scope.selectedEmployee); //fill in inputs with employee details
 
-    $http.get('https://gca-svcs01-dev.cloudapp.net/ClientService/getThumbnailImage?d=1').
+    $http.get('https://gca-svcs01-dev.cloudapp.net/ClientService/getThumbnailImage?d=26').
         success(function(data){
             console.log(data);
             $scope.getTemplate = data;
