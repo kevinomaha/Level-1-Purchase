84dbb01aaaabd69869eeb252a41f40acf97fa4f6
lsamban <lsamban@giftcertificates.com>
2014-11-11 16:37:26 -0600

    template display check4

--- app/js/controllers/GC/customizationCtrl.js
+++ app/js/controllers/GC/customizationCtrl.js
@@ -9,7 +9,7 @@ function ($routeParams, $sce, $scope, $451, $rootScope, $location, Customization
             console.log(data);
             $scope.getTemplate = data;
             console.log($scope.getTemplate);
-            $scope.thumbnail = $scope.getTemplate[ThumbnailUrl];
+            $scope.thumbnail = $scope.getTemplate[0].ThumbnailUrl;
             console.log($scope.thumbnail);
 
             //console.log($scope.getTemplate.ThumbNailUrl);
