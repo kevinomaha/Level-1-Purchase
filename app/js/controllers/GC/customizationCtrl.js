bd29531b431160d0c0b8b3f3106d3f67a43e96c6
lsamban <lsamban@giftcertificates.com>
2014-11-11 16:14:45 -0600

    template display check2

--- app/js/controllers/GC/customizationCtrl.js
+++ app/js/controllers/GC/customizationCtrl.js
@@ -9,7 +9,7 @@ function ($routeParams, $sce, $scope, $451, $rootScope, $location, Customization
             console.log(data);
             $scope.getTemplate = data;
             console.log($scope.getTemplate);
-            console.log($score.getTemplate.ThumbnailUrl)
+            console.log($scope.getTemplate.ThumbnailUrl)
 
             //console.log($scope.getTemplate.ThumbNailUrl);
 
