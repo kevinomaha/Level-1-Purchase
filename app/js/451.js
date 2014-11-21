632362700516f7f1accc09a37741c210ff408ef5
Kenn Thompson <kthompson@gcincentives.com>
2014-11-21 11:23:39 -0600

    API

--- app/js/451.js
+++ app/js/451.js
@@ -63,5 +63,7 @@ four51.app.factory('$451', ['AppConst', function(AppConst) {
 	};
 }]);
 four51.apiName = function(){
-	return four51.app.ApiAppName ? four51.app.ApiAppName : window.location.pathname.split('/')[1];
+    var _apiname = four51.app.ApiAppName ? four51.app.ApiAppName : window.location.pathname.split('/')[1];
+    console.log("ApiName:" + _apiname);
+    return _apiname;
 };
