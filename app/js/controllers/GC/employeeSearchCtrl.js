f9b4795260617db16105ae11852d3a201ecef455
lsamban <lsamban@giftcertificates.com>
2014-12-18 14:07:05 -0600

    digital

--- app/js/controllers/GC/employeeSearchCtrl.js
+++ app/js/controllers/GC/employeeSearchCtrl.js
@@ -189,10 +189,11 @@ four51.app.controller('EmployeeSearchCtrl', ['$routeParams', '$sce', '$scope', '
         function areRecipientReady() {
             console.log("inside areRecipientReady....");
             if ( ($scope.selectedProduct.ProductType == "Digital" || $scope.selectedProduct.ProductType == "e-Cards") && $scope.recipientList.length>0 ) {
-                //console.log($scope.selectedProduct.ProductType);
+                console.log($scope.selectedProduct.ProductType);
                 $scope.recipientsReady = true;
                 //console.log("new value of recipientready:" + $scope.recipientsReady);
                 angular.forEach($scope.recipientList.List, function(user){
+                    console.log("user is" + user);
                     if( user.EmailAddress.length<=0 )
                         $scope.recipientsReady = false;
                 });
