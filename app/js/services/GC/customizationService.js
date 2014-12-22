b1977560ce5b0295ec3e94800a343d1892c5debe
lsamban <lsamban@giftcertificates.com>
2014-12-22 13:32:06 -0600

    checking p/d-16-fix

--- app/js/services/GC/customizationService.js
+++ app/js/services/GC/customizationService.js
@@ -263,7 +263,7 @@ four51.app.factory('Customization', ['$451', '$http', 'ProductDescription',
 
         var _areRecipientsReady = function(recipientList, product, recipientsReady) {
             console.log("inside customization.areRecipientsReady");
-            recipientList = validateRecipientList(recipientList);
+            recipientList = _validateRecipientList(recipientList);
             if( (product.ProductType == "Digital" || product.ProductType == "e-Cards")&& recipientList.length>0 ) {
                 console.log(product.ProductType);
                 var j=0;
