bada37a22317bffa6710c08438b69d566f39f4e2
Lokesh <s_lokesh@outlook.com>
2014-12-24 01:05:31 -0600

    checkin-3

--- app/js/controllers/GC/customizationCtrl.js
+++ app/js/controllers/GC/customizationCtrl.js
@@ -37,6 +37,14 @@ four51.app.controller('CustomizationCtrl', ['$routeParams', '$sce', '$scope', '$
             });
         }
 
+        if ($scope.recipientList.List && $scope.selectedProduct.IsDigital) {
+            angular.forEach($scope.recipientList.List, function(recipient){
+                if(recipient.selected){
+                    $scope.selectedRecipients.push(recipient);
+                }
+            });
+        }
+
         //Automatically selecting all recipients - TP#12177
         /*$scope.selectRecipient = function(recipient) {
             if (!recipient.Valid) return;
