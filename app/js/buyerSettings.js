5eebd48af0b819dd71bb50172b4b550bca5d0b57
Kyle Olson <kolson@four51.com>
2015-01-26 14:29:37 -0600

    Setting recipient creation options to just Search by default

--- app/js/buyerSettings.js
+++ app/js/buyerSettings.js
@@ -1,7 +1,7 @@
 four51.app.constant("BuyerSettings",{
     'Recipients': {
         'Search':true,
-        'Upload':true,
+        'Upload':false,
         'Manual':false
     },
     'Logo':'http://www.four51.com/Themes/Custom/61a587a4-70db-45d4-a97a-5b54bef0d55a/GC_Main/logos/gc.jpg'
