aa9e20a266ef6b63ed6d1f5466cc3cdd8a35205c
Kyle Olson <kolson@four51.com>
2015-02-18 13:53:14 -0600

    #13171 SPA Future Ship Date format during order MM/DD/YYYY - setting minimum and maximum date for cart

--- app/js/directives/customtextfield.js
+++ app/js/directives/customtextfield.js
@@ -6,7 +6,15 @@ four51.app.directive('customtextfield', function() {
         },
         restrict: 'E',
         transclude: true,
-        templateUrl: 'partials/controls/customTextField.html'
+        templateUrl: 'partials/controls/customTextField.html',
+        controller: CustomTextFieldCtrl
     }
     return obj;
+
+    CustomTextFieldCtrl.$inject = ['$scope'];
+    function CustomTextFieldCtrl($scope) {
+        var today = new Date();
+        $scope.currentDate = angular.copy(today);
+        $scope.maxDate = today.setDate(today.getDate() + 120);
+    }
 });
